fdd1ff5f857aefd54141b3658ca59b3fa22a9bd6
Charles Srstka <support@charlessoft.com>
2021-07-25 00:42:54 -0500

    Add failureReason property to ConnectionError

--- Sources/CSAuthSampleApp/ConnectionError.swift
+++ Sources/CSAuthSampleApp/ConnectionError.swift
@@ -6,10 +6,28 @@
 //
 
 import Foundation
+import XPC
 
-enum ConnectionError: Error {
+public enum ConnectionError: LocalizedError {
     case connectionInterrupted
     case connectionInvalid
     case unexpectedConnection
     case unexpectedEvent
+
+    public var failureReason: String? {
+        switch self {
+        case .connectionInterrupted:
+            let rawError = xpc_copy_description(XPC_ERROR_CONNECTION_INTERRUPTED)
+            defer { free(UnsafeMutableRawPointer(mutating: rawError)) }
+            return String(cString: rawError)
+        case .connectionInvalid:
+            let rawError = xpc_copy_description(XPC_ERROR_CONNECTION_INVALID)
+            defer { free(UnsafeMutableRawPointer(mutating: rawError)) }
+            return String(cString: rawError)
+        case .unexpectedConnection:
+            return String(localized: "Unexpected XPC connection")
+        case .unexpectedEvent:
+            return String(localized: "Unexpected XPC event")
+        }
+    }
 }
