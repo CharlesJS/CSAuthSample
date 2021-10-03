2ef12f58316b15e842566a0fb2a107b499d25b64
Charles Srstka <support@charlessoft.com>
2021-10-03 18:05:09 -0400

    Bump SwiftyXPC

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "SwiftyXPC", url: "https://github.com/CharlesJS/SwiftyXPC.git", from: "0.2.4")
+        .package(name: "SwiftyXPC", url: "https://github.com/CharlesJS/SwiftyXPC.git", from: "0.2.6-beta")
     ],
     targets: [
         .target(
